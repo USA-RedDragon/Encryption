cfe325c49977e53d53f63afc885870652710e63b
Jacob McSwain <jacob.a.mcswain@gmail.com>
2015-12-03 17:55:26 -0600

    Reorder SMS display

--- app/src/main/java/com/mcswainsoftware/encryptionmessenger/SMS.java
+++ app/src/main/java/com/mcswainsoftware/encryptionmessenger/SMS.java
@@ -1,20 +1,22 @@
 package com.mcswainsoftware.encryptionmessenger;
 import java.util.*;
 
-public SMS implements Comparable<SMS>{
+public class SMS implements Comparable<SMS>{
     public long date;
     public String message;
-
+    public boolean me;
+    
   @Override
   public int compareTo(SMS o) {
-    if(o.date < date) return -1;
+    if(o.date > date) return -1;
     if(o.date == date) return 0;
-    if(o.date > date) return 1;
+    if(o.date < date) return 1;
     return -2;
   }
   
-  public SMS(String message, String date) {
+  public SMS(String message, String date, boolean me) {
       this.message = message;
       try { this.date = Long.parseLong(date); } catch(Exception ex) { ex.printStackTrace(); }
-  }
+      this.me = me;
+     }
 }

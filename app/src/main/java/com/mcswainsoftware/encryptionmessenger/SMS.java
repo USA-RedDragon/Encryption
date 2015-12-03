33ec135261b250cd80d38c49840115ba6b32307e
Jacob McSwain <jacob.a.mcswain@gmail.com>
2015-12-03 14:47:57 -0600

    Merge stuff

--- app/src/main/java/com/mcswainsoftware/encryptionmessenger/SMS.java
+++ app/src/main/java/com/mcswainsoftware/encryptionmessenger/SMS.java
@@ -1,18 +1,20 @@
 package com.mcswainsoftware.encryptionmessenger;
+import java.util.*;
 
 public SMS implements Comparable<SMS>{
-	public long date;
-	public String message;
-	
+    public long date;
+    public String message;
+
   @Override
   public int compareTo(SMS o) {
-	if(o.date < date) return -1;
+    if(o.date < date) return -1;
     if(o.date == date) return 0;
-	if(o.date > date) return 1;
+    if(o.date > date) return 1;
+    return -2;
   }
   
   public SMS(String message, String date) {
-	  this.message = message;
-	  try { this.date = Long.parseLong(date); } catch(Exception ex) { ex.printStackTrace(); }
+      this.message = message;
+      try { this.date = Long.parseLong(date); } catch(Exception ex) { ex.printStackTrace(); }
   }
 }

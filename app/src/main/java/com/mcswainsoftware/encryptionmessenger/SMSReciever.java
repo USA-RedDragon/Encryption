7e88c94b8306420c9ece2f15912ebe9bd1475296
Jacob McSwain <jacob.a.mcswain@gmail.com>
2015-12-03 21:07:39 -0600

    Add notification

--- app/src/main/java/com/mcswainsoftware/encryptionmessenger/SMSReciever.java
+++ app/src/main/java/com/mcswainsoftware/encryptionmessenger/SMSReciever.java
@@ -3,43 +3,54 @@ package com.mcswainsoftware.encryptionmessenger;
 import android.content.*;
 import android.provider.*;
 import android.telephony.*;
+import android.app.*;
+import android.graphics.drawable.*;
+import android.graphics.*;
 
 public class SMSReciever extends BroadcastReceiver { 
     @Override 
     public void onReceive(Context context, Intent intent) { 
         SmsMessage[] messages = Telephony.Sms.Intents.getMessagesFromIntent(intent);
-        Intent resultIntent = new Intent(this, ConversationActivity.class);
-        resultIntent.putExtra("number", Utilities.trimNumber(i.getOriginatingAddress()));
+        Intent resultIntent = new Intent(context, ConversationActivity.class);
+        resultIntent.putExtra("number", Utilities.trimNumber(messages[0].getOriginatingAddress()));
         resultIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | 
         Intent.FLAG_ACTIVITY_CLEAR_TASK);
-     
+    
 // Because clicking the notification launches a new ("special") activity, 
 // there's no need to create an artificial back stack.
 PendingIntent resultPendingIntent =
          PendingIntent.getActivity(
-         this,
+         context,
          0,
          resultIntent,
          PendingIntent.FLAG_UPDATE_CURRENT
 );
 
 // This sets the pending intent that should be fired when the user clicks the
-// notification. Clicking the notification launches a new activity.
-builder.setContentIntent(resultPendingIntent);
-Intent dismissIntent = new Intent(this, QuickReplyActivity.class);
+// notification. Clicking the notification launches a new activity
+
+Intent dismissIntent = new Intent(context, MainActivity.class);
 dismissIntent.setAction(Intent.ACTION_VIEW);
-PendingIntent piDismiss = PendingIntent.getService(this, 0, dismissIntent, 0);
+PendingIntent piDismiss = PendingIntent.getActivity(context, 0, dismissIntent, 0);
 
           for(SmsMessage i : messages) {
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
-        .setSmallIcon(R.drawable.ic_stat_notification)
-        .setContentTitle(Utilities.getContactName(context));
+              String name = Utilities.getContactName(context, i.getOriginatingAddress());
+              boolean boo=true;
+              if(name.isEmpty()) boo=false;
+              
+            Notification.Builder builder = new Notification.Builder(context)
+         .setSmallIcon(R.drawable.ic_send_white_18dp)
+        .setContentIntent(resultPendingIntent)
+        .setContentTitle((boo) ? name:i.getOriginatingAddress())
         .setContentText(i.getMessageBody())
         .setDefaults(Notification.DEFAULT_ALL) // requires VIBRATE permission
-        .setStyle(new NotificationCompat.BigTextStyle()
-                .bigText(msg))
-        .addAction (R.drawable.ic_stat_dismiss,
-                getString(R.string.dismiss), piDismiss);
+        .setStyle(new Notification.BigTextStyle()
+                .bigText(i.getDisplayMessageBody()))
+        .addAction (R.drawable.ic_send_white_18dp,
+                context.getString(R.string.delete), piDismiss);
+                NotificationManager nM=(NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+                nM.notify(001, builder.build());
           }
+         
     }
 }
--- app/src/main/java/com/mcswainsoftware/encryptionmessenger/Utilities.java
+++ app/src/main/java/com/mcswainsoftware/encryptionmessenger/Utilities.java
@@ -1,6 +1,10 @@
 package com.mcswainsoftware.encryptionmessenger;
+import android.net.*;
+import android.database.*;
+import android.content.*;
+import android.provider.*;
 
-public class Utlities {
+public class Utilities {
 
     public static String trimNumber(String num) {
             String trimnum = num.replaceAll("\\D+", "").trim();
@@ -9,12 +13,11 @@ public class Utlities {
     }
     
     public static String getContactName(Context context, String number) {
-    	Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, number));
+    	Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, number);
 	Cursor c = context.getContentResolver().query(uri, new String[] {
 				ContactsContract.PhoneLookup.DISPLAY_NAME, ContactsContract.PhoneLookup.PHOTO_THUMBNAIL_URI
 			}, null, null, null);
 			String name = "";
-			String uri2 = "";
 			if (c.moveToFirst()) {
 				name = c.getString(0);
 				c.moveToNext();
@@ -23,7 +26,7 @@ public class Utlities {
     }
     
     public static String getContactIconURI(Context context, String number) {
-    		Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, number));
+    		Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, number);
 	Cursor c = context.getContentResolver().query(uri, new String[] {
 				ContactsContract.PhoneLookup.PHOTO_THUMBNAIL_URI
 			}, null, null, null);
@@ -34,5 +37,5 @@ public class Utlities {
 			}
 			return uri2;
     }
-    }
+ 
 }

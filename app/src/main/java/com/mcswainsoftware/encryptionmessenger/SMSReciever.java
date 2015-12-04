051e2003a513a897fea32541c2aebd0c2054385d
Jacob McSwain <USA-RedDragon@users.noreply.github.com>
2015-12-03 20:14:23 -0600

    Update SMSReciever.java

--- app/src/main/java/com/mcswainsoftware/encryptionmessenger/SMSReciever.java
+++ app/src/main/java/com/mcswainsoftware/encryptionmessenger/SMSReciever.java
@@ -7,8 +7,26 @@ import android.telephony.*;
 public class SMSReciever extends BroadcastReceiver { 
     @Override 
     public void onReceive(Context context, Intent intent) { 
-          SmsMessage[] messages = Telephony.Sms.Intents.getMessagesFromIntent(intent);
-          System.out.println("MCSWAIN: Recieved SMS");    
+        SmsMessage[] messages = Telephony.Sms.Intents.getMessagesFromIntent(intent);
+        Intent resultIntent = new Intent(this, ConversationActivity.class);
+        resultIntent.putExtra("number", Utilities.trimNumber(i.getOriginatingAddress()));
+        resultIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | 
+        Intent.FLAG_ACTIVITY_CLEAR_TASK);
+     
+// Because clicking the notification launches a new ("special") activity, 
+// there's no need to create an artificial back stack.
+PendingIntent resultPendingIntent =
+         PendingIntent.getActivity(
+         this,
+         0,
+         resultIntent,
+         PendingIntent.FLAG_UPDATE_CURRENT
+);
+
+// This sets the pending intent that should be fired when the user clicks the
+// notification. Clicking the notification launches a new activity.
+builder.setContentIntent(resultPendingIntent);
+
           for(SmsMessage i : messages) {
              System.out.println("MCSWAIN: MESSAGE BODY: " + i.getMessageBody());
               System.out.println("MCSWAIN: MESSAGE FROM: "+ i.getOriginatingAddress());

c2b4fcab2a0edf52725fe92c59c8e8037a5e60c0
Jacob McSwain <USA-RedDragon@users.noreply.github.com>
2015-12-03 20:26:18 -0600

    Update Utilities.java

--- app/src/main/java/com/mcswainsoftware/encryptionmessenger/Utilities.java
+++ app/src/main/java/com/mcswainsoftware/encryptionmessenger/Utilities.java
@@ -7,4 +7,32 @@ public class Utlities {
 			if (trimnum.charAt(0) == '1') trimnum = trimnum.substring(1).trim();
 			return trimnum;
     }
+    
+    public static String getContactName(Context context, String number) {
+    	Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, number));
+	Cursor c = context.getContentResolver().query(uri, new String[] {
+				ContactsContract.PhoneLookup.DISPLAY_NAME, ContactsContract.PhoneLookup.PHOTO_THUMBNAIL_URI
+			}, null, null, null);
+			String name = "";
+			String uri2 = "";
+			if (c.moveToFirst()) {
+				name = c.getString(0);
+				c.moveToNext();
+			}
+			return name;
+    }
+    
+    public static String getContactIconURI(Context context, String number) {
+    		Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, number));
+	Cursor c = context.getContentResolver().query(uri, new String[] {
+				ContactsContract.PhoneLookup.PHOTO_THUMBNAIL_URI
+			}, null, null, null);
+			String uri2 = "";
+			if (c.moveToFirst()) {
+				uri2 = c.getString(0);
+				c.moveToNext();
+			}
+			return uri2;
+    }
+    }
 }

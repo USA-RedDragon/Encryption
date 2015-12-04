81dc856eb8161aaa63a9a0489c0791dc73f5e725
Jacob McSwain <USA-RedDragon@users.noreply.github.com>
2015-12-03 19:36:48 -0600

    Add SMS Reciever

---
+++ app/src/main/java/com/mcswainsoftware/encryptionmessenger/SMSReciever.java
@@ -0,0 +1,16 @@
+package com.mcswainsoftware.encryptionmessenger;
+
+import android.content.*;
+import android.provider.*;
+import android.telephony.*;
+
+public class SMSReciever extends BroadcastReceiver { 
+    @Override 
+    public void onReceive(Context context, Intent intent) { 
+          SmsMessage[] messages = Telephony.Sms.Intents.getMessagesFromIntent(intent);
+          System.out.println("MCSWAIN: Recieved SMS");    
+          for(SmsMessage i : messages) {
+             System.out.println("MCSWAIN: MESSAGE BODY: " + i.getMessageBody());
+          }
+    }
+}
